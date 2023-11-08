28e962d739e3c044b350822dfb69ba72834e5d0e
LcsCho <chojunhee98@gmail.com>
2023-11-08 14:13:41 +0900

    movie crud 작업

--- movie/src/main/java/com/kh/movie/dto/MovieDto.java
+++ movie/src/main/java/com/kh/movie/dto/MovieDto.java
@@ -1,5 +1,9 @@
 package com.kh.movie.dto;
 
+import java.sql.Date;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -8,7 +12,16 @@ import lombok.NoArgsConstructor;
 @Data @Builder @NoArgsConstructor @AllArgsConstructor
 public class MovieDto {
 	private int movieNo;
-	private String movieName, movieReleaseDate, movieDirector;
+	private String movieName, movieDirector;
+	private Date movieReleaseDate;
 	private int movieTime;
 	private String movieLevel, movieNation, movieContent;
+	
+	@JsonIgnore
+	public boolean isEmpty() {
+		return movieNo == 0 && movieName == null && movieReleaseDate == null
+				&& movieDirector == null && movieTime == 0
+				&& movieLevel == null && movieNation == null
+				&& movieContent == null;
+	}
 }
