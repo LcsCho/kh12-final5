d5424f05110f3d25b4f67e69c787ca9769dbef66
LcsCho <chojunhee98@gmail.com>
2023-11-09 16:19:47 +0900

    MemberDao 오류 해결

--- movie/src/main/java/com/kh/movie/dao/MemberDao.java
+++ movie/src/main/java/com/kh/movie/dao/MemberDao.java
@@ -6,5 +6,7 @@ public interface MemberDao {
 	void insert(MemberDto memberDto);
 	MemberDto selectOne(String memberId);
 	MemberDto login(MemberDto memberDto);
+	int getCount();
+	void updateMemberInfo(MemberDto inputDto);
 
 }
