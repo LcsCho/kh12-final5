da658bd7e7db722eb0632084dfb9d5c8cd1524f7
wkdwns0501 <britain0501@naver.com>
2023-11-16 13:46:10 +0900

    오류 수정

--- movie/src/main/java/com/kh/movie/controller/HomeController.java
+++ movie/src/main/java/com/kh/movie/controller/HomeController.java
@@ -1,14 +1,14 @@
 package com.kh.movie.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 	
-
 	@RequestMapping("/")
-	public String main() {
+	public String main(Model model) {
 		return "main";
 	}
 
