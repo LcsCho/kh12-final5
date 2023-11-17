0a50da780ed5df4758a650870aa9b1e7c053d2ad
wkdwns0501 <britain0501@naver.com>
2023-11-17 09:52:32 +0900

    메인 기본 디자인 구현중

--- movie/src/main/java/com/kh/movie/controller/HomeController.java
+++ movie/src/main/java/com/kh/movie/controller/HomeController.java
@@ -1,14 +1,30 @@
 package com.kh.movie.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.kh.movie.dao.MovieDao;
+import com.kh.movie.dao.MovieGenreDao;
+import com.kh.movie.dto.MovieDto;
+import com.kh.movie.dto.MovieGenreDto;
+
 @Controller
 public class HomeController {
 	
+	@Autowired
+	private MovieDao movieDao;
+	
+	@Autowired
+	private MovieGenreDao movieGenreDao;
+	
 	@RequestMapping("/")
 	public String main(Model model) {
+		List<MovieDto> movieList = movieDao.selectList();
+		model.addAttribute("movieList", movieList);
 		return "main";
 	}
 
