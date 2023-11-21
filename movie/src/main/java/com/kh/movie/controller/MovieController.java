8f19c756ff01df13a8605c8949247c31dab3fcd2
oursidle <oursidle@naver.com>
2023-11-21 18:20:25 +0900

    리뷰 수정 구현 완료
    - 리뷰 수정 완료 시 리뷰 바로 변경될 수 있도록 추후 처리 필요

--- movie/src/main/java/com/kh/movie/controller/MovieController.java
+++ movie/src/main/java/com/kh/movie/controller/MovieController.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -148,5 +150,4 @@ public class MovieController {
 			return "redirect:에러페이지";
 		}
 	}
-
 }
